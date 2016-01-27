a3a73af55493e0a809c8e17e96d1b214c0ab826c
admin <liuyw_os@sari.ac.cn>
2016-01-27 16:19:46 +0800

    find preorder or postorder by others

--- tree.c
+++ tree.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct struc_node
 {
@@ -23,6 +24,7 @@ char g_pre_tree[32] = {0};
 char g_in_tree[32] = {0};
 char g_post_tree[32] = {0};
 int inc = 0;
+int g_len = 0;
 
 /* 按先序次序输入二叉树中节点的值，以空字符表示空树  */
 int create_bitree(P_TREENODE *T)
@@ -60,7 +62,7 @@ int pre_order_traverse(P_TREENODE T)
         return -1;
     }
 
-    printf("%c ", T->data);
+//    printf("%c ", T->data);
 
     //存储到数组中
     g_pre_tree[g_len] = T->data;
@@ -82,7 +84,7 @@ int in_order_traverse(P_TREENODE T)
 
     in_order_traverse(T->l_child);
 
-    printf("%c ", T->data);
+ //   printf("%c ", T->data);
     //存储到数组中
     g_in_tree[g_len] = T->data;
     g_len ++;
@@ -104,7 +106,7 @@ int post_order_traverse(P_TREENODE T)
 
     post_order_traverse(T->r_child);
 
-    printf("%c ", T->data);
+  //  printf("%c ", T->data);
     //存储到数组中
     g_post_tree[g_len] = T->data;
     g_len ++;
@@ -147,6 +149,68 @@ int count_leaf_num(P_TREENODE T)
     return 0;
 }
 
+//根据先序和中序求后序
+P_TREENODE  post_by_pre_and_in(char *preorder, char *inorder, int len)
+{
+    int i = 0;
+    int j = 0;
+    char c = 0;
+
+    if(0 >= len)
+    {
+        return NULL;
+    }
+
+    c = preorder[0];
+
+    for(i=0; i<len; i++)
+    {
+        if(c == inorder[i])
+            break;
+    }
+    j = i + 1;
+
+    P_TREENODE T = (P_TREENODE)malloc(sizeof(TREENODE));
+
+    T -> data = c;
+
+    T->l_child = post_by_pre_and_in(preorder + 1, inorder, i);
+    T->r_child = post_by_pre_and_in(preorder + j, inorder + j, len - j);
+
+    return T;
+}
+
+//根据后序和中序求先序
+P_TREENODE pre_by_post_and_in(char *postorder, char *inorder, int len)
+{
+    int i = 0;
+    int j = 0;
+    char c = 0;
+
+    if(0 >= len)
+    {
+        return NULL;
+    }
+
+    c = postorder[len - 1];
+
+    for(i=0; i<len; i++)
+    {
+        if(c == inorder[i])
+            break;
+    }
+    j = i + 1;
+
+    P_TREENODE T = (P_TREENODE)malloc(sizeof(TREENODE));
+
+    T -> data = c;
+
+    T -> l_child = pre_by_post_and_in(postorder, inorder, i);
+    T -> r_child = pre_by_post_and_in(postorder + i, inorder + j, len - j);
+
+    return T;
+}
+
 int main(void)
 {
 //    initscr();
@@ -166,6 +230,8 @@ int main(void)
         printf("*  3:  Traverse the Binary tree by back order    *\n");
         printf("*  4:  Count the node num of the Binary tree     *\n");
         printf("*  5:  Count the leaf node num of the Binary tree*\n");
+        printf("*  6:  find postorder by preorder and inorder*\n");
+        printf("*  7:  find preorder by postorder and inorder*\n");
         printf("**********************************************\n");
 
         printf("please input your choice:");
@@ -181,7 +247,7 @@ int main(void)
 
                 pre_order_traverse(test_tree);
 
-                printf("g_pre_tree is: %s\n", g_pre_tree);
+                printf("\ng_pre_tree is: %s\n", g_pre_tree);
                 printf("\n");
 
                 break;
@@ -195,7 +261,7 @@ int main(void)
 
                 in_order_traverse(test_tree);
 
-                printf("g_in_tree is: %s\n", g_in_tree);
+                printf("\ng_in_tree is: %s\n", g_in_tree);
                 printf("\n");
 
                 break;
@@ -209,7 +275,7 @@ int main(void)
 
                 post_order_traverse(test_tree);
 
-                printf("g_post_tree is: %s\n", g_post_tree);
+                printf("\ng_post_tree is: %s\n", g_post_tree);
                 printf("\n");
 
                 break;
@@ -229,17 +295,72 @@ int main(void)
                 break;
             }
 
+            case 6:
+            {
+                // 求出preorder 序列
+                g_len = 0;
+                memset(g_pre_tree, 0, sizeof(g_pre_tree));
+                pre_order_traverse(test_tree);
+                printf("\ng_pre_tree is: %s\n", g_pre_tree);
+
+                // 求出inorder 序列
+                g_len = 0;
+                memset(g_in_tree, 0, sizeof(g_in_tree));
+                in_order_traverse(test_tree);
+                printf("\ng_in_tree is: %s\n", g_in_tree);
+
+                // 构建新的二叉树
+                P_TREENODE new_tree = NULL;
+
+                new_tree = post_by_pre_and_in(g_pre_tree, g_in_tree, strlen(g_in_tree));
+
+                g_len = 0;
+                memset(g_post_tree, 0, sizeof(g_post_tree));
+                printf("\ng_post_tree is: %s\n", g_post_tree);
+                post_order_traverse(new_tree);
+                printf("\ng_post_tree is: %s\n", g_post_tree);
+
+                break;
+            }
+
+            case 7:
+            {
+                // 求出postorder 序列
+                g_len = 0;
+                memset(g_post_tree, 0, sizeof(g_post_tree));
+                post_order_traverse(test_tree);
+                printf("\ng_post_tree is: %s\n", g_post_tree);
+
+                // 求出inorder 序列
+                g_len = 0;
+                memset(g_in_tree, 0, sizeof(g_in_tree));
+                in_order_traverse(test_tree);
+                printf("\ng_in_tree is: %s\n", g_in_tree);
+
+                // 构建新的二叉树
+                P_TREENODE post_tree = NULL;
+
+                post_tree = pre_by_post_and_in(g_post_tree, g_in_tree, strlen(g_in_tree));
+
+                // 求出preorder 序列
+                g_len = 0;
+                memset(g_pre_tree, 0, sizeof(g_pre_tree));
+                printf("\ng_pre_tree is: %s\n", g_pre_tree);
+                pre_order_traverse(post_tree);
+                printf("\ng_pre_tree is: %s\n", g_pre_tree);
+
+                break;
+            }
             default:
                 break;
         }
 
         printf("please input any char to go on\n");
         getchar();
-    }while((1<=i) && (6 > i));
+    }while((1<=i) && (8 > i));
 
     getchar();
 
 //    endwin();
     return 0;
 }
-
